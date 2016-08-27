43faf476eb2e73ae1ad029f58758601014f790f6
Kai Luo <gluokai@gmail.com>
2016-08-27 17:14:07 +0800

    change var names

--- triangle.h
+++ triangle.h
@@ -20,16 +20,16 @@ struct Triangle: public Shape {
     a(a), b(b), c(c), n(((b-a)^(c-a)).Normalize()) {}
   ~Triangle() {}
   boost::optional<RayIntersection> Intersect(const Ray& ray) const {
-    auto E = ray.o - a;
-    auto B = b-a;
-    auto C = c-a;
-    auto det = Determinant3x3(B, C, -ray.d);
+    auto e = ray.o - a;
+    auto ba = b-a;
+    auto ca = c-a;
+    auto det = Determinant3x3(ba, ca, -ray.d);
     if (det == 0) return {};
-    auto beta = Determinant3x3(E, C, -ray.d) / det;
+    auto beta = Determinant3x3(e, ca, -ray.d) / det;
     if (beta < 0 || beta > 1) return {};
-    auto gamma = Determinant3x3(B, E, -ray.d) / det;
+    auto gamma = Determinant3x3(ba, e, -ray.d) / det;
     if (gamma < 0 || gamma > 1 - beta) return {};
-    auto t = Determinant3x3(B, C, E) / det;
+    auto t = Determinant3x3(ba, ca, e) / det;
     if (t < ray.tmin || t > ray.tmax) return {};
     return RayIntersection{ .t = t, .shape = this };
   }
