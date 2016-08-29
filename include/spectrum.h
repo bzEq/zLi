15748cb168eaa6a90e73692128298b6c0b08b1cb
Kai Luo <gluokai@gmail.com>
2016-08-29 15:57:40 +0800

    add spectrum class

---
+++ include/spectrum.h
@@ -0,0 +1,65 @@
+#ifndef _ZLI_SPECTRUM_H_
+#define _ZLI_SPECTRUM_H_
+#include "geometry.h"
+
+#include <iostream>
+#include <cmath>
+
+
+namespace zLi {
+
+const int nrSpectrumSamples = 4;
+struct Spectrum {
+  Float s[nrSpectrumSamples];
+  Spectrum() { for (int i = 0; i < nrSpectrumSamples; i++) s[i] = 0; }
+  Spectrum(const Float v) { for (int i = 0; i < nrSpectrumSamples; i++) s[i] = v; }
+  Spectrum(const Spectrum& sp) { for (int i = 0; i < nrSpectrumSamples; i++) s[i] = sp.s[i]; }
+  Spectrum(const Float v[nrSpectrumSamples]) { for (int i = 0; i < nrSpectrumSamples; i++) s[i] = v[i]; }
+  Spectrum& operator+=(const Spectrum& sp) { for (int i = 0; i < nrSpectrumSamples; i++) s[i] += sp.s[i]; }
+  Spectrum& operator*=(const Spectrum& sp) { for (int i = 0; i < nrSpectrumSamples; i++) s[i] *= sp.s[i]; }
+  Spectrum& operator*=(const Float f) { assert(f >= 0); for (int i = 0; i < nrSpectrumSamples; i++) s[i] *= f; }
+  Spectrum& operator/=(const Float f) { assert(f > 0); for (int i = 0; i < nrSpectrumSamples; i++) s[i] /= f; }
+  Float NormSquared() const { Float sum = 0; for (int i = 0; i < nrSpectrumSamples; i++) sum += s[i]*s[i]; return sum; }
+  Float Norm() const { return std::sqrt(NormSquared()); }
+};
+
+inline Spectrum operator+(const Spectrum& a, const Spectrum& b) {
+  Spectrum c;
+  for (int i = 0; i < nrSpectrumSamples; i++)
+    c.s[i] = a.s[i] + b.s[i];
+  return c;
+}
+
+inline Spectrum operator*(const Spectrum& a, const Spectrum& b) {
+  Spectrum c;
+  for (int i = 0; i < nrSpectrumSamples; i++)
+    c.s[i] = a.s[i] * b.s[i];
+  return c;
+}
+
+inline Spectrum operator*(const Spectrum& a, const Float f) {
+  assert(f >= 0);
+  Spectrum c;
+  for (int i = 0; i < nrSpectrumSamples; i++)
+    c.s[i] = a.s[i] * f;
+  return c;
+}
+
+inline Spectrum operator*(const Float f, const Spectrum& a) {
+  Spectrum c;
+  for (int i = 0; i < nrSpectrumSamples; i++)
+    c.s[i] = a.s[i] * f;
+  return c;
+}
+
+inline Spectrum operator/(const Spectrum& a, const Float f) {
+  assert(f > 0);
+  Spectrum c;
+  for (int i = 0; i < nrSpectrumSamples; i++)
+    c.s[i] = a.s[i] / f;
+  return c;
+}
+
+
+}
+#endif
