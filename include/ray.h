59f778eac933782803af86f614e9dc021126fc2a
Kai Luo <gluokai@gmail.com>
2016-08-29 18:00:06 +0800

    better indent

--- include/ray.h
+++ include/ray.h
@@ -10,7 +10,8 @@ struct Ray {
   Vector3f o, d;
   Float tmin, tmax;
   Ray() {}
-  Ray(const Vector3f& o, const Vector3f& d, const Float tmin=0, const Float tmax=INF): o(o), d(d), tmin(tmin), tmax(tmax) {}
+  Ray(const Vector3f& o, const Vector3f& d, const Float tmin=0, const Float tmax=INF)
+    : o(o), d(d), tmin(tmin), tmax(tmax) {}
   Vector3f operator()(const Float t) const { return o + t * d; }
 };
 
