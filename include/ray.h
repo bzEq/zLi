536ecae6808f5d30bbdbac3fa9d81b87b82f2c5b
Kai Luo <gluokai@gmail.com>
2016-08-29 17:59:27 +0800

    add default value for parameters

--- include/ray.h
+++ include/ray.h
@@ -10,8 +10,7 @@ struct Ray {
   Vector3f o, d;
   Float tmin, tmax;
   Ray() {}
-  Ray(const Vector3f& o, const Vector3f& d): o(o), d(d), tmin(0), tmax(INF) {}
-  Ray(const Vector3f& o, const Vector3f& d, const Float tmin, const Float tmax): o(o), d(d), tmin(tmin), tmax(tmax) {}  
+  Ray(const Vector3f& o, const Vector3f& d, const Float tmin=0, const Float tmax=INF): o(o), d(d), tmin(tmin), tmax(tmax) {}
   Vector3f operator()(const Float t) const { return o + t * d; }
 };
 
