b956c387fd1422c37cc319166e0836cbad3c9371
Kai Luo <gluokai@gmail.com>
2016-10-16 16:09:19 +0800

    minor fix

--- include/geometry.hh
+++ include/geometry.hh
@@ -23,7 +23,7 @@ struct Vector4;
 template<typename T>
 struct Vector3 {
   T x, y, z;
-  Vector3(): x(0), y(0), z(0) {}
+  Vector3() = default;
   Vector3(const T& x, const T& y, const T& z): x(x), y(y), z(z) {}
   bool IsZero() const { return x == 0 && y == 0 && z == 0; }
   T LengthSquared() const { return x * x + y * y + z * z; }
@@ -40,7 +40,7 @@ struct Vector3 {
   }
   Vector3& operator-=(const Vector3<T>& v) {
     x -= v.x; y -= v.y; z -= v.z;
-    return *this;    
+    return *this;
   }
   Vector3& operator*=(const T f) {
     x *= f; y *= f; z *= f;
@@ -57,13 +57,13 @@ struct Vector4 {
   T x, y, z, w;
   Vector4(): x(0), y(0), z(0), w(0) {}
   Vector4(const T x, const T y, const T z, const T w): x(x), y(y), z(z), w(w) {}
-  Vector4(const Vector3<T>& v, const T w): 
+  Vector4(const Vector3<T>& v, const T w):
     x(v.x), y(v.y), z(v.z), w(w) {}
   std::optional<Vector3<T>> ToVector3() const {
     if (w == 0) return {};
     return Vector3<T>(x/w, y/w, z/w);
   }
-  bool HasNaNs() const { return std::isnan(x) || std::isnan(y) || std::isnan(z) || std::isnan(w); }  
+  bool HasNaNs() const { return std::isnan(x) || std::isnan(y) || std::isnan(z) || std::isnan(w); }
   T operator[](int i) const { assert(i >= 0 && i < 4); return (&x)[i]; }
   T& operator[](int i) { assert(i >= 0 && i < 4); return (&x)[i]; }
 };
@@ -71,17 +71,17 @@ struct Vector4 {
 template<typename T>
 struct Matrix4x4 {
   T m[4][4];
-  
+
   Matrix4x4() {
     m[0][0] = m[1][1] = m[2][2] = m[3][3] = 1;
     m[0][1] = m[0][2] = m[0][3] = m[1][0] = m[1][2] = m[1][3] = m[2][0] =
-      m[2][1] = m[2][3] = m[3][0] = m[3][1] = m[3][2] = 0;    
+      m[2][1] = m[2][3] = m[3][0] = m[3][1] = m[3][2] = 0;
   }
-  
+
   Matrix4x4(const T mm[4][4]) {
     std::memcpy(m, mm, 16*sizeof(T));
   }
-  
+
   Matrix4x4(const T m00, const T m01, const T m02, const T m03,
             const T m10, const T m11, const T m12, const T m13,
             const T m20, const T m21, const T m22, const T m23,
@@ -91,28 +91,28 @@ struct Matrix4x4 {
     m[2][0]=m20; m[2][1]=m21; m[2][2]=m22; m[2][3]=m23;
     m[3][0]=m30; m[3][1]=m31; m[3][2]=m32; m[3][3]=m33;
   }
-  
+
   Matrix4x4 Transpose() const {
     return Matrix4x4(m[0][0], m[1][0], m[2][0], m[3][0],
                      m[0][1], m[1][1], m[2][1], m[3][1],
                      m[0][2], m[1][2], m[2][2], m[3][2],
                      m[0][3], m[1][3], m[2][3], m[3][3]);
   }
-  
+
   Matrix4x4 operator*(const Matrix4x4& rhs) const {
     Matrix4x4 r;
     for (int i = 0; i < 4; i++) {
       for (int j = 0; j < 4; j++) {
-        r.m[i][j] = 
+        r.m[i][j] =
           m[i][0] * rhs.m[0][j] +
           m[i][1] * rhs.m[1][j] +
           m[i][2] * rhs.m[2][j] +
           m[i][3] * rhs.m[3][j];
       }
     }
-    return r;    
+    return r;
   }
-  
+
   std::optional<Matrix4x4> Inverse() const {
     T minv[4][4];
     int ind[4] = { 0, 1, 2, 3 };
@@ -132,13 +132,13 @@ struct Matrix4x4 {
         }
       }
       std::swap(ind[i], ind[pick]);
-      
+
       T f = 1. / minv[i][i];
       minv[i][i] = 1.;
       for (int j = 0; j < 4; j++) {
         minv[i][j] *= f;
       }
-      
+
       for (int j = 0; j < 4; j++) {
         if (i != j) {
           T old = minv[j][i];
@@ -147,7 +147,7 @@ struct Matrix4x4 {
         }
       }
     }
-    
+
     for (int i = 0; i < 4; ) {
       if (ind[i] != i) {
         for (int j = 0; j < 4; j++) {
@@ -183,15 +183,15 @@ struct Quaternion {
   }
   Quaternion& operator/=(const T f) {
     r /= f; i /= f; j /= f; k /= f;
-    return *this;    
+    return *this;
   }
-  T operator[](int i) const { 
+  T operator[](int i) const {
     assert(i >= 0 && i < 4);
     return (&r)[i];
   }
   T& operator[](int i) {
     assert(i >= 0 && i < 4);
-    return (&r)[i];    
+    return (&r)[i];
   }
   Quaternion Conjugation() const { return Quaternion(r, -i, -j, -k); }
   T NormSquared() const {
@@ -200,7 +200,7 @@ struct Quaternion {
   T Norm() const {
     return std::sqrt(NormSquared());
   }
-  Quaternion Inverse() const { 
+  Quaternion Inverse() const {
     assert(!IsZero());
     return Conjugation() / NormSquared();
   }
@@ -245,9 +245,9 @@ inline Vector3<T> operator-(const Vector3<T>& u, const Vector3<T>& v) {
 
 template<typename T>
 inline T operator*(const Vector3<T>& u, const Vector3<T>& v) {
-  return 
-    u.x * v.x + 
-    u.y * v.y + 
+  return
+    u.x * v.x +
+    u.y * v.y +
     u.z * v.z;
 }
 
