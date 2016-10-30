2ea2bb5cafad1c6f5650c6ca99a20b7b234077ef
Kai Luo <gluokai@gmail.com>
2016-10-30 14:44:10 +0800

    minor change

--- include/kdtree.hh
+++ include/kdtree.hh
@@ -12,42 +12,49 @@ namespace zLi {
 
 struct KdNode;
 
-struct Todo {
-  KdNode *node;
-  Float tmin, tmax;
-  Todo(KdNode *node, Float tmin, Float tmax)
-      : node(node), tmin(tmin), tmax(tmax) {}
-};
-
-struct KdNode {
-  int axis;
-  Float d;
-  BoundBox box;
-  KdNode *child[2];
-  int Side(const Vector3f &p) const { return p[axis] > d; }
-  int OtherSide(const Vector3f &p) const { return !Side(p); }
-  KdNode(const BoundBox &box) : axis(-1), d(0), box(box) {
-    child[0] = nullptr;
-    child[1] = nullptr;
-  }
-  KdNode(int axis, Float d, const BoundBox &box) : axis(axis), d(d), box(box) {
-    child[0] = nullptr;
-    child[1] = nullptr;
-  }
-  std::optional<std::tuple<Float, Float>> Intersect(const Ray &ray) const {
-    return box.Intersect(ray);
-  }
-  ~KdNode() {
-    delete child[0];
-    delete child[1];
-  }
-};
-
-struct KdTree {
-  KdNode root;
+class KdTree {
+public:
   KdTree() : root(BoundBox()) {}
   KdTree(const std::vector<BoundBox> &boxList);
   void Traverse(const Ray &ray);
+
+private:
+  struct KdNode;
+  struct Todo;
+
+  struct Todo {
+    KdNode *node;
+    Float tmin, tmax;
+    Todo(KdNode *node, Float tmin, Float tmax)
+        : node(node), tmin(tmin), tmax(tmax) {}
+  };
+
+  struct KdNode {
+    int axis;
+    Float d;
+    BoundBox box;
+    KdNode *child[2];
+    int Side(const Vector3f &p) const { return p[axis] > d; }
+    int OtherSide(const Vector3f &p) const { return !Side(p); }
+    KdNode(const BoundBox &box) : axis(-1), d(0), box(box) {
+      child[0] = nullptr;
+      child[1] = nullptr;
+    }
+    KdNode(int axis, Float d, const BoundBox &box)
+        : axis(axis), d(d), box(box) {
+      child[0] = nullptr;
+      child[1] = nullptr;
+    }
+    std::optional<std::tuple<Float, Float>> Intersect(const Ray &ray) const {
+      return box.Intersect(ray);
+    }
+    ~KdNode() {
+      delete child[0];
+      delete child[1];
+    }
+  };
+
+  KdNode root;
 };
 
 } // end namespace zLi
