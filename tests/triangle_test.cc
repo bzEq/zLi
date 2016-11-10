af36a56083c994e5bad761914c5ed577209903f6
Kai Luo <gluokai@gmail.com>
2016-11-10 21:04:49 +0800

    use shared_ptr

--- tests/triangle_test.cc
+++ tests/triangle_test.cc
@@ -1,16 +1,15 @@
 #include "triangle.hh"
 
+#include <memory>
 
-int main()
-{
+int main() {
   using namespace zLi;
   Vector3f v(-1, -1, 0);
-  Triangle tri(Vector3f(0,0,2) + v,
-               Vector3f(3,0,2) + v,
-               Vector3f(0,4,2) + v);
-  Ray r(Vector3f(0,0,0),
-        Vector3f(0,0,1));
-  auto test = tri.Intersect(r);
-  if (test) std::cout << (*test).t << std::endl;
+  auto tri = std::make_shared<Triangle>(
+      Vector3f(0, 0, 2) + v, Vector3f(3, 0, 2) + v, Vector3f(0, 4, 2) + v);
+  Ray r(Vector3f(0, 0, 0), Vector3f(0, 0, 1));
+  auto test = tri->Intersect(r);
+  if (test)
+    std::cout << (*test).t << std::endl;
   return 0;
 }
