544b628249e952ac12816ea1238019272931ad20
Kai Luo <gluokai@gmail.com>
2016-11-10 21:05:38 +0800

    use random functions from libstdc++

--- include/sample.hh
+++ include/sample.hh
@@ -3,37 +3,38 @@
 #include "math.hh"
 
 #include <cstdlib>
+#include <random>
 #include <tuple>
-
 
 namespace zLi {
 
 inline Float UniformSample() {
-  static unsigned short xi[3] = { 43, 37, 73 };
-  return static_cast<Float>(erand48(xi));
+  static std::random_device rd;
+  static std::mt19937 g(rd());
+  static std::uniform_real_distribution<Float> u(0, 1);
+  return u(g);
 }
 
 inline Vector3f SampleFromHemiSphere() {
-  Float u = 2*PI*UniformSample();
+  Float u = 2 * PI * UniformSample();
   Float v = UniformSample();
-  return Vector3f(std::cos(u)*std::sqrt(v),
-                  std::sin(u)*std::sqrt(v),
-                  std::sqrt(1-v));
+  return Vector3f(std::cos(u) * std::sqrt(v), std::sin(u) * std::sqrt(v),
+                  std::sqrt(1 - v));
 }
 
 inline Vector3f SampleFromSphere() {
   Vector3f v = SampleFromHemiSphere();
-  return UniformSample() <= 0.5 ? v: -v;
+  return UniformSample() <= 0.5 ? v : -v;
 }
 
 inline std::tuple<Float, Float> SampleFromDisk() {
   Float r = UniformSample();
-  Float u = 2*PI*UniformSample();
-  return std::make_tuple(r*std::cos(u), r*std::sin(u));
+  Float u = 2 * PI * UniformSample();
+  return std::make_tuple(r * std::cos(u), r * std::sin(u));
 }
 
 inline std::tuple<Float, Float> SampleFromCircle() {
-  Float u = 2*PI*UniformSample();
+  Float u = 2 * PI * UniformSample();
   return std::make_tuple(std::cos(u), std::sin(u));
 }
 
