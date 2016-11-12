9ad24b7f582350c22739e342df3bf227a522531b
Kai Luo <gluokai@gmail.com>
2016-11-12 16:32:55 +0800

    change class name

--- src/diagram2d.cc
+++ src/diagram2d.cc
@@ -4,27 +4,27 @@
 
 namespace zLi {
 
-Diagram2d::Diagram2d(const Float *xs, const Float *ys, size_t l) {
+Diagram2D::Diagram2D(const Float *xs, const Float *ys, size_t l) {
   for (size_t i = 0; i < l; ++i) {
     diagram_.push_back(std::make_tuple(xs[i], ys[i]));
   }
   std::sort(diagram_.begin(), diagram_.end());
 }
 
-Diagram2d::Diagram2d(const Float *xs, Float y, size_t l) {
+Diagram2D::Diagram2D(const Float *xs, Float y, size_t l) {
   for (size_t i = 0; i < l; ++i) {
     diagram_.push_back(std::make_tuple(xs[i], y));
   }
   std::sort(diagram_.begin(), diagram_.end());
 }
 
-void Diagram2d::Display(std::ostream &out) {
+void Diagram2D::Display(std::ostream &out) {
   for (const auto &p : diagram_) {
     out << "(" << std::get<0>(p) << ", " << std::get<1>(p) << ")\n";
   }
 }
 
-Float Diagram2d::Query(Float x) const {
+Float Diagram2D::Query(Float x) const {
   auto iter = std::lower_bound(
       diagram_.begin(), diagram_.end(), std::make_tuple(x, 0),
       [](const std::tuple<Float, Float> &a, const std::tuple<Float, Float> &b) {
@@ -39,62 +39,62 @@ Float Diagram2d::Query(Float x) const {
               std::get<0>(diagram_[j]), std::get<1>(diagram_[j]), x);
 }
 
-Diagram2d &Diagram2d::operator+=(const Diagram2d &rhs) {
+Diagram2D &Diagram2D::operator+=(const Diagram2D &rhs) {
   for (auto &p : diagram_) {
     std::get<1>(p) += rhs.Query(std::get<0>(p));
   }
   return *this;
 }
 
-Diagram2d &Diagram2d::operator+=(Float d) {
+Diagram2D &Diagram2D::operator+=(Float d) {
   for (auto &p : diagram_) {
     std::get<1>(p) += d;
   }
   return *this;
 }
 
-Diagram2d &Diagram2d::operator*=(const Diagram2d &rhs) {
+Diagram2D &Diagram2D::operator*=(const Diagram2D &rhs) {
   for (auto &p : diagram_) {
     std::get<1>(p) *= rhs.Query(std::get<0>(p));
   }
   return *this;
 }
 
-Diagram2d &Diagram2d::operator*=(Float f) {
+Diagram2D &Diagram2D::operator*=(Float f) {
   for (auto &p : diagram_) {
     std::get<1>(p) *= f;
   }
   return *this;
 }
 
-Diagram2d operator*(Float f, const Diagram2d &rhs) {
-  Diagram2d res(rhs);
+Diagram2D operator*(Float f, const Diagram2D &rhs) {
+  Diagram2D res(rhs);
   for (auto &p : res.diagram_) {
     std::get<1>(p) *= f;
   }
   return res;
 }
 
-Diagram2d operator*(const Diagram2d &lhs, Float f) { return operator*(f, lhs); }
-Diagram2d operator*(const Diagram2d &lhs, const Diagram2d &rhs) {
-  Diagram2d res(lhs);
+Diagram2D operator*(const Diagram2D &lhs, Float f) { return operator*(f, lhs); }
+Diagram2D operator*(const Diagram2D &lhs, const Diagram2D &rhs) {
+  Diagram2D res(lhs);
   for (auto &p : res.diagram_) {
     std::get<1>(p) *= rhs.Query(std::get<0>(p));
   }
   return res;
 }
 
-Diagram2d operator+(Float f, const Diagram2d &rhs) {
-  Diagram2d res(rhs);
+Diagram2D operator+(Float f, const Diagram2D &rhs) {
+  Diagram2D res(rhs);
   for (auto &p : res.diagram_) {
     std::get<1>(p) += f;
   }
   return res;
 }
 
-Diagram2d operator+(const Diagram2d &lhs, Float f) { return operator*(f, lhs); }
-Diagram2d operator+(const Diagram2d &lhs, const Diagram2d &rhs) {
-  Diagram2d res(lhs);
+Diagram2D operator+(const Diagram2D &lhs, Float f) { return operator*(f, lhs); }
+Diagram2D operator+(const Diagram2D &lhs, const Diagram2D &rhs) {
+  Diagram2D res(lhs);
   for (auto &p : res.diagram_) {
     std::get<1>(p) += rhs.Query(std::get<0>(p));
   }
