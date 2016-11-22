d7f3f7ab191683c2ff620936e5bd69939e488584
Kai Luo <gluokai@gmail.com>
2016-11-22 19:53:12 +0800

    support for variant length log buffer

--- include/logging.hh
+++ include/logging.hh
@@ -2,25 +2,49 @@
 #define _ZLI_LOGGING_HH_
 #include <string>
 
-#define LOG(level, format, ...)                                         \
+#define LOG(level, format, ...)                                                \
   zLi::Logging(level, __FILE__, __FUNCTION__, __LINE__, format, ##__VA_ARGS__)
 
-#define INFO(format, ...)                                               \
-  zLi::Logging(zLi::INFO, __FILE__, __FUNCTION__, __LINE__, format, ##__VA_ARGS__)
+#define INFO(format, ...)                                                      \
+  zLi::Logging(zLi::INFO, __FILE__, __FUNCTION__, __LINE__, format,            \
+               ##__VA_ARGS__)
 
-#define DEBUG(format, ...)                                              \
-  zLi::Logging(zLi::DEBUG, __FILE__, __FUNCTION__, __LINE__, format, ##__VA_ARGS__)
+#define DEBUG(format, ...)                                                     \
+  zLi::Logging(zLi::DEBUG, __FILE__, __FUNCTION__, __LINE__, format,           \
+               ##__VA_ARGS__)
 
-#define WARN(format, ...)                                               \
-  zLi::Logging(zLi::WARN, __FILE__, __FUNCTION__, __LINE__, format, ##__VA_ARGS__)
+#define WARN(format, ...)                                                      \
+  zLi::Logging(zLi::WARN, __FILE__, __FUNCTION__, __LINE__, format,            \
+               ##__VA_ARGS__)
 
-#define ERROR(format, ...)                                              \
-  zLi::Logging(zLi::ERROR, __FILE__, __FUNCTION__, __LINE__, format, ##__VA_ARGS__)
+#define ERROR(format, ...)                                                     \
+  zLi::Logging(zLi::ERROR, __FILE__, __FUNCTION__, __LINE__, format,           \
+               ##__VA_ARGS__)
 
-#define FATAL(format, ...)                                              \
-  zLi::Logging(zLi::FATAL, __FILE__, __FUNCTION__, __LINE__, format, ##__VA_ARGS__)
+#define FATAL(format, ...)                                                     \
+  zLi::Logging(zLi::FATAL, __FILE__, __FUNCTION__, __LINE__, format,           \
+               ##__VA_ARGS__)
 
 namespace zLi {
+
+template <typename... Args>
+inline std::string FormatToString(const char *fmt, Args &&... args) {
+  static const int LIMITS = (1 << 16);
+  int cap = 64;
+  char *buf = (char *)malloc(cap);
+  while (cap <= LIMITS) {
+    int len = snprintf(buf, cap, fmt, std::forward<Args>(args)...);
+    if (len < cap) {
+      break;
+    }
+    free(buf);
+    cap <<= 1;
+    buf = (char *)malloc(cap);
+  }
+  std::string s(buf);
+  free(buf);
+  return s;
+}
 
 enum LogLevel { INFO, DEBUG, WARN, ERROR, FATAL };
 
@@ -29,22 +53,34 @@ extern const char *LLStr[5];
 
 void SetLogLevel(LogLevel);
 
-void Logging(LogLevel level, const char* file, const char* func, int line,
-             std::string&& msg);
+void Logging(LogLevel level, const char *file, const char *func, int line,
+             std::string &&msg);
 
+void Logging(LogLevel level, const char *file, const char *func, int line,
+             const char *msg);
 
-void Logging(LogLevel level, const char* file, const char* func, int line,
-             const char* msg);
-
-template<typename ...Args>
-static void Logging(LogLevel level, const char* file, const char* func, int line,
-                    const char* fmt, Args&&... args) {
-  if (level < LL) return;
-  char buf[1024];
-  std::snprintf(buf, sizeof(buf), fmt, std::forward<Args>(args)...);
-  return Logging(level, file, func, line, buf);
+template <typename... Args>
+inline static void Logging(LogLevel level, const char *file, const char *func,
+                           int line, const char *fmt, Args &&... args) {
+  static const int LIMITS = (1 << 16);
+  if (level < LL) {
+    return;
+  }
+  int cap = 64;
+  char *buf = (char *)malloc(cap);
+  while (cap <= LIMITS) {
+    int len = snprintf(buf, cap, fmt, std::forward<Args>(args)...);
+    if (len < cap) {
+      break;
+    }
+    free(buf);
+    cap <<= 1;
+    buf = (char *)malloc(cap);
+  }
+  Logging(level, file, func, line, buf);
+  free(buf);
+  return;
 }
-
 } // namespace zLi
 
 #endif
