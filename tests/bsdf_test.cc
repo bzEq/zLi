7602b9c860774b1b1441940bf0506a34505348fa
Kai Luo <gluokai@gmail.com>
2016-11-23 21:43:47 +0800

    test for bsdf

--- tests/bsdf_test.cc
+++ tests/bsdf_test.cc
@@ -1,5 +1,6 @@
 #include "bsdf.hh"
 
+#include <cmath>
 #include <iostream>
 
 using namespace zLi;
@@ -7,7 +8,7 @@ using namespace zLi;
 int main() {
   BSDF r = Refractive::ImplBSDF(1.2);
   Vector3f n(0, 1, 0);
-  Vector3f wi(Vector3f(-1, 1, 0).Normalize());
+  Vector3f wi(Vector3f(-1 / std::sqrt(1.2 * 1.2 - 1), 1, 0).Normalize());
   auto t = r.btdf->pdf(n, wi);
   std::cout << std::get<0>(t) << std::endl;
   std::cout << std::get<1>(t) << std::endl;
