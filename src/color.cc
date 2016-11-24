5167561edb75786b6dfcdcbad440e33899e9b918
Kai Luo <gluokai@gmail.com>
2016-11-24 21:08:24 +0800

    minor fix

--- src/color.cc
+++ src/color.cc
@@ -6,54 +6,22 @@ namespace zLi {
 
 Float &XYZColor::operator[](int i) {
   assert(i >= 0 && i < 3);
-  switch (i) {
-  case 0:
-    return x;
-  case 1:
-    return y;
-  case 2:
-    return z;
-  }
-  return x;
+  return (&x)[i];
 }
 
 Float XYZColor::operator[](int i) const {
   assert(i >= 0 && i < 3);
-  switch (i) {
-  case 0:
-    return x;
-  case 1:
-    return y;
-  case 2:
-    return z;
-  }
-  return x;
+  return (&x)[i];
 }
 
 Float &RGBColor::operator[](int i) {
   assert(i >= 0 && i < 3);
-  switch (i) {
-  case 0:
-    return r;
-  case 1:
-    return g;
-  case 2:
-    return b;
-  }
-  return r;
+  return (&r)[i];
 }
 
 Float RGBColor::operator[](int i) const {
   assert(i >= 0 && i < 3);
-  switch (i) {
-  case 0:
-    return r;
-  case 1:
-    return g;
-  case 2:
-    return b;
-  }
-  return r;
+  return (&r)[i];
 }
 
 RGBColor RGBColor::sRGB() const {
