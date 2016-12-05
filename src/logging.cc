d34f642124ea1c0cc35cdc7e38265a0dae9630c3
Kai Luo <gluokai@gmail.com>
2016-12-05 15:52:07 +0800

    minor fix

--- src/logging.cc
+++ src/logging.cc
@@ -11,7 +11,7 @@ Logger::LogLevel Logger::CurrentLevel = Logger::INFO;
 const char *Logger::LogLevelString[5] = {"INFO", "DEBUG", "WARN", "ERROR",
                                          "FATAL"};
 
-void Logger::SetLogLevel(Logger::LogLevel lv) { CurrentLevel = lv; }
+void Logger::SetLogLevel(Logger::LogLevel level) { CurrentLevel = level; }
 
 void Logger::Logging(Logger::LogLevel level, const char *file, const char *func,
                      int line, const char *msg) {
